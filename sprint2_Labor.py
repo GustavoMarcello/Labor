94f10b78dcd96b5b6106ce51ac80c627ca3059d1
GustavoMarcello <61936009+GustavoMarcello@users.noreply.github.com>
2021-05-22 15:24:47 -0300

    v2, já treinando e subindo dados ao banco
    melhorias: imprimir um gráfico com a curva de aprendizado

--- sprint2_Labor.py
+++ sprint2_Labor.py
@@ -1,16 +1,10 @@
-#Dataset utilizado: Labor
-#Esse Dataset foi usado para aprender as descrições de contratos aceitos e não aceitos
+# Dataset utilizado: Labor. Esse Dataset foi usado para aprender as descrições de contratos aceitos e não aceitos
+# Este programa acessa o DataSet, faz a tratativa dos dados, selecionando 2 de seus treasures,
+#plota um grafico com os dados já tratados, depois treina esses dados e armazena os resultados no MongoDB
 
-#*******descobrir como testar e treinar o código, a partir das aulas da alura*********
-
-from re import X
-from typing import NamedTuple
-from numpy import NaN
+import numpy as np
 from pandas.core.algorithms import mode
 import pymongo
-from scipy.sparse import data
-from sklearn import datasets
-import sklearn
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -19,98 +13,78 @@ import openml
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#conectando ao servidor/conectando ao banco/criando coleção
-myClient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myClient["labor"]
-myCol = mydb["contratos"]
-
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#buscando dataset: Labor https://www.openml.org/d/4
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# buscando dataset: Labor https://www.openml.org/d/4
 dataset = openml.datasets.get_dataset(4)
-#printando informações do dataset
-#print(dataset)
+# printando informações do dataset
+print(dataset)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#adicionando as informações do dataset a variável info
-x, y, categorical_indicator, attribute_names = dataset.get_data(dataset_format="dataframe", target=dataset.default_target_attribute)
-# print(y)
-#isolando coluna standby-py
-standby_pay = x['standby-pay']
-#print(standby_pay)
-#isolando coluna duration
-duration = x['duration']
-#print(duration)
-#isolando classe
-classe = y
-#print(classe)
-#print(classe.shape) #verificando quantidade de dados
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# adicionando as informações do dataset a variável info
+x, y, categorical_indicator, attribute_names = dataset.get_data(
+    dataset_format="dataframe", target=dataset.default_target_attribute)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#cria o gráfico
-sns.scatterplot(x=standby_pay, y=duration, hue=classe, data=x)
-#imprime o gráfico
-#plt.show()
-
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#ajustando dados
-
-#alterando tipo de y: Series para DataFrame
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# ajustando dados
+# alterando tipo de y: Series para DataFrame
 y_DF = pd.DataFrame(y)
-#print(y_DF)
-#criando dados somente com as colunas desejadas
+# criando dados somente com as colunas desejadas
 dados = x[["duration", "standby-pay"]]
-#print(dados)
-#unindo y_DF com dados
+# unindo y_DF com dados
 dados['class'] = y_DF
-#print(dados)
-#excluindo "standby-pay" = NaN
+# excluindo "standby-pay" = NaN
 dados = dados.dropna()
 print(dados)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#começando treino
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# gera o gráfico
+sns.scatterplot(x="duration", y="standby-pay", hue="class", data=dados)
+plt.show()
 
-# #separando dados x e y
-# raw_dados_x = x[["duration", "standby-pay"]]
-# # print(raw_dados_x)
-# raw_dados_y = pd.DataFrame(y) #transformando y de Series para DataFrame
-# # print(raw_dados_y)
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# treinos e testes
 
-# raw_dados_x['class'] = raw_dados_y
-# #print(raw_dados_x)
+# separando dados x e y
+x = dados[["duration", "standby-pay"]]
+# print(dados_x)
+y = dados["class"]
+# print(dados_y)
 
 # #verificando tipos de dados raw_dados_x/raw_dados_y
-# print(type(raw_dados_x))
+# print(type(x))
 # print('')
-# print(type(raw_dados_y))
+# print(type(y))
 
-# #excluindo "standby-pay" = NaN
-# dados_x = raw_dados_x.dropna()
-# print(dados_x.shape)
+#definindo semente fixa para train_test_split e LinearSVC
+SEED = 5
+np.random.seed(SEED)
 
-
-# dados_x = []
-# for k in len(raw_dados_x):
-#     if raw_dados_x["standby-pay"] != NaN:
-#         dados_x.append(k)
-# print(dados_x)
-
-
-# #qtde de treino (total 57)
-# treino_x = dados_x[:43]
-# treino_y = dados_y[:43]
-# #qtde de teste (total 57)
-# teste_y = dados_x[43:]
-# teste_x = dados_y[43:]
-# print(f'Treinaremos com {len(treino_x)} e testaremos com {len(teste_x)} elementos')
+treino_x, teste_x, treino_y, teste_y = train_test_split(
+    x, y, test_size=0.3, stratify=y)
+print(
+    f'Treinaremos com {len(treino_x)} e testaremos com {len(teste_x)} elementos')
 
 # #rodando e treinando o modelo
-# modelo = LinearSVC()
-# modelo.fit(treino_x, treino_y)
-# previsoes = modelo.predict(teste_x)
-# acuracia = accuracy_score(teste_y, previsoes) * 100
-# print(f'A acurácia foi de {acuracia:.2f}')
+modelo = LinearSVC()
+modelo.fit(treino_x, treino_y.values.ravel())  # convertendo para uma dimensão
+previsoes = modelo.predict(teste_x)
+acuracia = accuracy_score(teste_y, previsoes) * 100
+print(f'A acurácia foi de {acuracia:.2f} %')
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#inserindo valores no banco
 
+#conectando ao servidor/conectando ao banco/criando coleção
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myClient["labor"]
+mycol = mydb["contratos"]
+
+dic = {"treinados": len(treino_x),"testados": len(teste_x), "acuracia": acuracia}
+mycol.insert_one(dic)
+
+#visualizando os valores
+for x in mycol.find():
+    print(x)
+print('')
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
